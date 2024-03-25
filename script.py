55431e165e25a9180a0b71f7933ad47d3a8116c7
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-03-25 02:26:12 +0100

    modified:   input.txt 	modified:   output.txt 	modified:   script.py

--- script.py
+++ script.py
@@ -1,15 +1,15 @@
 import os
 
-def count_unique_g_keys(input_file, output_file):
+def countUnique(input_file, output_file):
     unique_keys = set()  
 
     
     with open(input_file, 'r') as file:
         for line in file:
             
-            if "g-key:" in line:
+            if "gcid:" in line:
                 
-                key = line.split("g-key:")[1].strip()
+                key = line.split("gcid:")[1].strip()
                 unique_keys.add(key)  
 
     
@@ -18,12 +18,12 @@ def count_unique_g_keys(input_file, output_file):
 
     
     with open(output_file, 'w') as out_file:
-        out_file.write(f"Liczba unikalnych g-keys: {len(unique_keys)}\n")
-        out_file.write("Lista unikalnych g-keys:\n")
+        out_file.write(f"Liczba unikalnych gcid: {len(unique_keys)}\n")
+        out_file.write("\nLista unikalnych gcid:\n")
         for key in unique_keys:
             out_file.write(key + '\n')
 
 current_directory = os.getcwd()
 input_file_path = os.path.join(current_directory, "input.txt")  # Ścieżka do pliku wejściowego
 output_file_path = os.path.join(current_directory, "output.txt")  # Ścieżka do pliku wyjściowego
-count_unique_g_keys(input_file_path, output_file_path)
+countUnique(input_file_path, output_file_path)
